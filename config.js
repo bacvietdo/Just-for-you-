f59c8f5b1812716af5ab6b4e28d0d0ba723546cd
bacvietdo <100986320+bacvietdo@users.noreply.github.com>
2022-03-05 15:54:20 +0900

    Update and rename css to config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    introTitle: 'Hé luu Phưn :3',
+    introDesc: Đây là món quà tặng em đó, em click tiếp đi nè ^^,
+    desc: 'Mong là em thích á ><',
+    btnIntro: 'Oke ạ',
+    title: 'Em có thích anh chưa nè 😉',
+    btnYes: 'Kó',
+    btnNo: 'Hum thèm 🙄',
+    question:'A trêu chút thui ý :3 Dù em thấy thế nào thì a cx mãi iu em thui 🥰, em tin anh hum nè hehe ^^',
+    btnReply: 'Gửi anh',
+    reply: 'Yêu thì yêu mà không yêu thì yêu hi ^^'
+    mess: 'Dạ zâng em iu 🥰. Yêu em nhắm lun ><',
+    messDesc: 'Mừng em ngày Quốc tế Phụ nữ 8/3 nhe 😍 Chúc em lun mạnh khỏe và lun vui vẻ, hạnh phúc, yêu đời á 😊 Mong là em thích món quà nho nhỏ nì 😅',
+    btnAccept: 'Meo meo :333',
+    messLink: 'https://m.me/tobyyy.kr'

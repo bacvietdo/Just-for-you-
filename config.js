a1dff8ae5cc905e80579086451c94d992a15156d
bacvietdo <100986320+bacvietdo@users.noreply.github.com>
2022-03-05 15:55:30 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     introTitle: 'Hé luu Phưn :3',
     introDesc: Đây là món quà tặng em đó, em click tiếp đi nè ^^,
-    desc: 'Mong là em thích á ><',
+    desc: 'Mong là em thích món quà nho nhỏ nì á ><',
     btnIntro: 'Oke ạ',
     title: 'Em có thích anh chưa nè 😉',
     btnYes: 'Kó',
@@ -10,6 +10,6 @@ const CONFIG = {
     btnReply: 'Gửi anh',
     reply: 'Yêu thì yêu mà không yêu thì yêu hi ^^'
     mess: 'Dạ zâng em iu 🥰. Yêu em nhắm lun ><',
-    messDesc: 'Mừng em ngày Quốc tế Phụ nữ 8/3 nhe 😍 Chúc em lun mạnh khỏe và lun vui vẻ, hạnh phúc, yêu đời á 😊 Mong là em thích món quà nho nhỏ nì 😅',
+    messDesc: 'Mừng em ngày Quốc tế Phụ nữ 8/3 nhe 😍 Chúc em lun mạnh khỏe và lun vui vẻ, hạnh phúc, yêu đời á 😊',
     btnAccept: 'Meo meo :333',
     messLink: 'https://m.me/tobyyy.kr'
